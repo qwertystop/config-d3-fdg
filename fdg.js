15f8a0cbce7832032fb57ff39c2d513c994497ce
David Heyman <dheyman@fastmail.com>
2018-02-23 14:34:45 -0500

    Reorganization and nulls

--- fdg.js
+++ fdg.js
@@ -28,33 +28,19 @@ let dColScales = {// for each column, specify a function from value to color
 }
 
 // set up contents of dropdowns
-// TODO include a null
 d3.select("#config").selectAll("select")
 	.selectAll("option")
-	.data(Object.keys(dIntScales))
+	.data(["---"].concat(Object.keys(dIntScales)))
 	.enter()
 	.append("option")
 	.attr("value", d => {return d})
 	.text(d => {return d})
 
-
-let fX = d3.forceX((node, _index) => {
-	return dIntScales[eXConf.value](node[eXConf.value]) * dWidth})
-
-let fY = d3.forceX((node, _index) => {
-	return dIntScales[eYConf.value](node[eYConf.value]) * dHeight})
-
-let fLink = d3.forceLink()
-
 function mid(lo, hi, un) {
 	return Math.max(lo, Math.min(un, hi)) }
 
-// load data and run page
-d3.csv("data.csv", (error, tData) => {
-	if (error) throw error
-
-	// Now the SVG part
-	// draw nodes
+// node setup
+function nodeSetup() {
 	let eNodes = eSvg.append("g")
 		.attr("class", "nodes")
 		.selectAll("circle")
@@ -64,54 +50,99 @@ d3.csv("data.csv", (error, tData) => {
 		.attr("r", 5)
 		.attr("fill", d => {
 			return dColScales[eColorConf.value](d[eColorConf.value])})
-
-	eColorConf.onchange = () => {
-		eNodes.transition().attr("fill", d => {
-			return dColScales[eColorConf.value](d[eColorConf.value])})}
-
 	// TODO hover labelling
 	// TODO click for details-in-sidebar
+
+	return eNodes }
+
+// calculate links
+function calcLinks(key, eNodes) {
+	// for each node
+	let bunched = eNodes.nodes().map((n, i, arr) => {
+		// only compare to things not yet covered
+		arr.slice(i+1)
+		// match to each other node with the same value
+		// in the field used for linking
+			.filter(o => { return o[key] === n[key]})
+		// and produce link objects
+			.map(o => { return {"source": n, "target": o}})})
+	// finally, flatten it
+	return [].concat(...bunched)}
+
+function defOnchanges(eNodes, eLinks, sim) {
+	eLinkConf.onChange = () => {
+		let key = this.value
+		if (key === "---") {
+			sim.force("fLink", null)
+			eLinks.data([]).exit().remove()}
+		else {
+			let aLinks = calcLinks(this.value, eNodes)
+			sim.force("fLink", d3.forceLink(aLinks))
+			eLinks.data(aLinks).enter().append("line")}
+		sim.alpha(1)
+		sim.restart()}
+
+	eColorConf.onchange = () => {
+		let key = this.value
+		if (key === "---") {
+			eNodes.transition().attr("fill", "black")}
+		else {
+			let mapper = dColScales[key]
+			eNodes.transition().attr("fill", d => {
+				return mapper(d[key])})}}
+
+	eXConf.onChange = () => {
+		let key = this.value
+		if (key === "---") {
+			sim.force("fX", null)
+		} else {
+			let mapper = dIntScales[key]
+			sim.force("fX", d3.forceX((d) => {
+				mapper(d[key]) * dWidth }))}
+		}
+		sim.alpha(1)
+		sim.restart()}
+
+	eYConf.onChange = () => {
+		let key = this.value
+		if (key === "---") {
+			sim.force("fY", null)
+		} else {
+			let mapper = dIntScales[key]
+			sim.force("fY", d3.forceX((d) => {
+				mapper(d[key]) * dHeight }))}
+		}
+		sim.alpha(1)
+		sim.restart()} }
+
+// load data and run page
+d3.csv("data.csv", (error, tData) => {
+	if (error) throw error
+
+	// Now the SVG part
+	let eNodes = nodeSetup()
 
 	// deletion
 	eNodes.exit().remove()
 
-	// calculate links
-	function calcLinks() {
-		let sLVal = eLinkConf.value
-		// for each node
-		let bunched = eNodes.nodes().map((n, i, arr) => {
-			// only compare to things not yet covered
-			arr.slice(i+1)
-			// match to each other node with the same value
-			// in the field used for linking
-				.filter(o => { return o[sLVal] === n[sLVal]})
-			// and produce link objects
-				.map(o => { return {"source": n, "target": o}})})
-		// finally, flatten it
-		return [].concat(...bunched)}
-
 	// draw links
-	let eLink = eSvg.append("g")
+	let eLinks = eSvg.append("g")
 		.attr("class", "links")
 		.selectAll("line")
-	// data will be added later
-
-	// adding
-	eLink.enter().append("line")
-
+		.enter().append("line")
 	// removing
 	eLink.exit().remove()
+	// data will be added in dropdown onchange
 
-	// forces
+	// make the simulation
 	let simulation = d3.forceSimulation().nodes(tData)
-	eLinkConf.onchange = () => {
-		let aLinks = calcLinks()
-		fLink.links(aLinks)
-		eLink.data(aLinks)}
 
+	// non-configured forces
 	simulation.force("fCharge", d3.forceManyBody())
 		.force("fCenter", d3.forceCenter(dWidth/2, dHeight/2))
-		.force("fX", fX).force("fY", fY).force("fLink", fLink)
+
+	// set up onchange for selection boxes
+	defOnchanges(eNodes, eLinks, simulation)
 
 	function onTick() {
 		// copy position updates from simnode to svg element
@@ -120,12 +151,10 @@ d3.csv("data.csv", (error, tData) => {
 			.attr("cy", d => {return mid(0, dHeight, d.y)})
 
 		// and the same for links
-		eLink.attr("x1", d => {return d.source.x})
+		eLinks.attr("x1", d => {return d.source.x})
 			.attr("y1", d => {return d.source.y})
 			.attr("x2", d => {return d.target.x})
 			.attr("y2", d => {return d.target.y})}
 
 	simulation.on("tick", onTick)
-
-	// TODO CANDY automatic cluster labels?
 })
